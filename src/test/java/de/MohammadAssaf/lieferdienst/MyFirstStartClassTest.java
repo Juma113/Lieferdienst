2720d53224f2b3935b631c8a19b89e8e8ace190a
mhassaf <73823787+mhassaf@users.noreply.github.com>
2020-11-09 22:38:28 +0100

    My First Java Program.

---
+++ src/test/java/de/MohammadAssaf/lieferdienst/MyFirstStartClassTest.java
@@ -0,0 +1,13 @@
+package de.MohammadAssaf.lieferdienst;
+
+class MyFirstStartClassTest {
+
+    void check_if_something_does_something() {
+        // Given
+
+        // When
+
+        // Then
+    }
+}
+
