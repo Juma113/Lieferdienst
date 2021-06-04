670a4bd22a2fa5b65d0c5e9e60ed29f65e2ad47b
mhassaf <73823787+mhassaf@users.noreply.github.com>
2021-06-04 20:59:10 +0200

    add OrderNotFoundException

---
+++ src/main/java/de/lieferdienst/controllers/orderManagment/OrderNotFoundAdvice.java
@@ -0,0 +1,20 @@
+package de.lieferdienst.controllers.orderManagment;
+
+
+import de.lieferdienst.model.errors.OrderNotFounfException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class OrderNotFoundAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(OrderNotFounfException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String personNotFoundHandler(OrderNotFounfException ex) {
+        return ex.getMessage();
+    }
+}
---
+++ src/main/java/de/lieferdienst/controllers/orderManagment/OrdersController.java
@@ -0,0 +1,85 @@
+package de.lieferdienst.controllers.orderManagment;
+
+import de.lieferdienst.model.errors.OrderNotFounfException;
+import de.lieferdienst.model.helper.Address;
+import de.lieferdienst.model.orderManagment.Orders;
+import de.lieferdienst.model.orderManagment.Payment;
+import de.lieferdienst.model.orderManagment.ShoppingCart;
+import de.lieferdienst.repository.storage.AddressRepository;
+import de.lieferdienst.repository.storage.OrdersRepository;
+import de.lieferdienst.repository.storage.ShoppingCartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Transactional
+@RestController
+@RequestMapping("/api/order")
+public class OrdersController {
+
+    private final OrdersRepository ordersRepository;
+
+    private final AddressRepository addressRepository;
+
+    private final ShoppingCartRepository shoppingCartRepository;
+
+    @Autowired
+    public OrdersController(OrdersRepository ordersRepository, AddressRepository addressRepository, ShoppingCartRepository shoppingCartRepository) {
+        this.ordersRepository = ordersRepository;
+        this.addressRepository = addressRepository;
+        this.shoppingCartRepository = shoppingCartRepository;
+    }
+
+
+    @GetMapping("/default")
+    ResponseEntity<List<Orders>> getAllOrders()
+    {
+        return ResponseEntity.ok(this.ordersRepository.findAll());
+    }
+
+    @GetMapping(path = "/{id}")
+    ResponseEntity<Orders> findOrderByID(@PathVariable(value = "id") Long id) throws OrderNotFounfException
+    {
+        return ResponseEntity.ok(this.ordersRepository
+                .findById(id)
+                .orElseThrow(() -> new OrderNotFounfException("No Order found for id " + id)));
+    }
+
+    @PostMapping(path = "/add", produces = "application/json")
+    ResponseEntity<Orders> addNewOrder(@RequestParam Long addressId, @RequestParam Long shoppingCartId) {
+        Address address = addressRepository.findById(addressId).get();
+        ShoppingCart shoppingcart = shoppingCartRepository.findById(shoppingCartId).get();
+        Orders order = new Orders();
+        order.setShippingAddress(address);
+        order.setShoppingcart(shoppingcart);
+        order.setPayment(new Payment());
+        return ResponseEntity.ok(this.ordersRepository.save(order));
+    }
+
+    @PutMapping(path = "/update/{id}", produces = "application/json")
+    ResponseEntity<Orders> updateOrder(@PathVariable Long id, @RequestBody Orders newOrder) {
+        return ordersRepository.findById(id)
+                .map(order -> {
+                    order.setOrdered(newOrder.getOrdered());
+                    order.setShipped(newOrder.getShipped());
+                    order.setShippingAddress(newOrder.getShippingAddress());
+                    order.setOrderStatus(newOrder.getOrderStatus());
+                    order.setShippingStatus(newOrder.getShippingStatus());
+                    order.setShoppingcart(newOrder.getShoppingcart());
+                    order.setPayment(newOrder.getPayment());
+                    return ResponseEntity.ok(this.ordersRepository.save(order));
+                })
+                .orElseGet(() -> {
+                    newOrder.setId(id);
+                    return ResponseEntity.ok(this.ordersRepository.save(newOrder));
+                });
+    }
+
+    @DeleteMapping("/delete/{id}")
+    void deleteOrder(@PathVariable Long id) {
+        this.ordersRepository.deleteById(id);
+    }
+}
