d0deaa6244e975b445d59a8379dec4617d54bebe
mhassaf <73823787+mhassaf@users.noreply.github.com>
2021-06-28 18:15:40 +0200

    UserDetails

---
+++ src/main/java/de/lieferdienst/repository/storage/UserDetail/SecurityDetailsService.java
@@ -0,0 +1,29 @@
+package de.lieferdienst.repository.storage.UserDetail;
+
+import de.lieferdienst.model.userManagment.User;
+import de.lieferdienst.repository.storage.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+@Service
+public class SecurityDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByEmail(email);
+
+        user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + user.isPresent()));
+        //SecurityUserDetails securityUserDetails = new SecurityUserDetails(user.get());
+        //return securityUserDetails;
+        return user.map(SecurityUserDetails::new).get();
+
+    }
+}
