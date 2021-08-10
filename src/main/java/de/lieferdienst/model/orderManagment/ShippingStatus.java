2b5ce14bc8494b832f06fdf5d125a0cfd7250575
mhassaf <73823787+mhassaf@users.noreply.github.com>
2021-08-10 20:12:49 +0200

    Update ShippingStatus

--- src/main/java/de/lieferdienst/model/orderManagment/ShippingStatus.java
+++ src/main/java/de/lieferdienst/model/orderManagment/ShippingStatus.java
@@ -3,8 +3,8 @@ package de.lieferdienst.model.orderManagment;
 public enum ShippingStatus {
 
     PENDING,
-    SENT,
-    ON_THE_WAY,
-    DELIVERED,
-    RETURNED
+    GESENDET,
+    AUF_DEM_WEG,
+    GELIEFERT,
+    ZURÜCKGESCHEKT
 }
